e077c0cfa78b34c1f49ee1dedeb91d1542f2ed0f
Constanza Abarca <costi.abarca@gmail.com>
2018-10-11 13:51:40 -0400

    Codigo chequea edad. Pruebas pasan.

--- classPension.py
+++ classPension.py
@@ -16,3 +16,18 @@ class Pension():
         dias = (hoy-fechaTrabajo).days
         semanas = dias/7
         return round(semanas)
+    
+    def esPensionado (self, genero, dia, mes, anio, edad, cond):
+        
+        semanas = self.calcularSemanas(dia, mes, anio)
+        
+        if (genero == "M"):
+            if (edad < 60):
+                return False 
+        elif (genero == "F"):
+            if (edad < 55):
+                return False
+        if (semanas > 750):
+            return True
+        else:
+            return False
