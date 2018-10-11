c88c14a0d019b3a94deaee792751991551d0c8c5
Constanza Abarca <costi.abarca@gmail.com>
2018-10-11 13:34:41 -0400

    Craeda primera prueba. Chequea calculo semanas. Falla.

--- classPension.py
+++ classPension.py
@@ -5,3 +5,10 @@ Creado el 10 oct. 2018
 @author: Pedro Maldona 13-10790
 
 '''
+
+import datetime
+
+class Pension():
+    
+    def calcularSemanas(self, dia, mes, anio):
+        pass
--- testPension.py
+++ testPension.py
@@ -6,6 +6,17 @@ Creado el 10 oct. 2018
 
 '''
 
+from classPension import *
+import unittest
 
+
+
+class testPension(unittest.TestCase):
+    
+    def setUp(self):
+        self.pension = Pension()
+        
+    def testCalcularSemanas(self):
+        self.assertEqual(self.pension.calcularSemanas(5, 4, 1990), 1488, "Se deben obtener 1488 semanas.")
 
     
